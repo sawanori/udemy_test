ae89a015ba0265cc1a1be3be933d807331f17ce3
noritaka saweada <snp.inc.info@gmail.com>
2019-10-31 09:59:10 +0900

    useStatemaster

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, {useState} from 'react';
 
 const App = (props) => {
- const [name,setName] = useState(props.name)
- const [price,setPrice] = useState(props.price)
+ const [state,setState] = useState(props)
+
  const nameChange = (e) => {
-   setName(e.target.value)
+   setState({...state, name: e.target.value})
  }
  const reset = () => {
-   setPrice(props.price)
-   setName(props.name)
+   setState(props)
  }
+const {price,name}= state
   return (
     <>
       <p>現在の{name}は{price}</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setState({...state, price : price + 1 })}>+1</button>
+      <button onClick={() => setState({...state,price:price - 1})}>-1</button>
       <button onClick={reset}>Reset</button>
       <input type="text" value={name} onChange={nameChange}/>
     </>
