1528a7a8dc8611177359bd100b6596e2bf356a4c
noritaka saweada <snp.inc.info@gmail.com>
2019-10-31 09:27:00 +0900

    setCounbt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+ const [count,setCount] =  useState(1)
+ const increment = () => {
+   setCount(count + 1)
+ }
+
+ const increment2= () => {
+   setCount(previousCount => previousCount + 1)
+ }
+ const decrement2 = () => {
+   setCount(previousCount => previousCount -1)
+ }
+
+ const decrement = () => {
+   setCount(count - 1)
+ }
+ const clear = () => {
+   setCount(0)
+ }
+ const double = () => {
+   setCount(count * 2)
+ }
+
+ const triple = () => {
+   setCount(previousCount =>
+   previousCount % 3 === 0 ?   previousCount / 3  :   previousCount
+   )
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>{count}</h1>
+    <button onClick={increment}>+</button>
+    <button onClick={increment2}>+</button>
+    <button onClick={decrement}>-</button>
+    <button onClick={decrement2}>-</button>
+    <button onClick = {double}>double</button>
+      <button onClick = {triple}>triple</button>
+    <button onClick={clear}>clear</button>
+    </>
+  )
 }
-
 export default App;
